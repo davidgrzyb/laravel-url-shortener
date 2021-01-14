80641bd035f3ffbf02a2c13c0b7aa5a9fe9eb4df
David Grzyb <grzybdavid@gmail.com>
2021-01-13 22:55:42 -0500

    Create Link model, migration, factory & seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function links()
+    {
+        return $this->hasMany(Link::class);
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Link;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+            ->has(Link::factory()->count(500))
+            ->create([
+                'name' => 'User',
+                'email' => 'user@test.com',
+            ]);
     }
 }
